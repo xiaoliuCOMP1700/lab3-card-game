c95c7b352232edf84f045c46e272d29849ae5ab4
xiaoliuCOMP1700 <xliu43@studentmail.nait.ca>
2017-04-17 11:46:51 -0600

    initial setup

---
+++ lab3 card game/lab3 card game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_card_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
